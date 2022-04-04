da1e32fbbb0a356a15ee54136431cde5d2df4784
fabrazzak <freelancerabdurrazzak47@gmail.com>
2022-04-04 15:55:50 +0600

    update about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className='container my-5 py-5'>
             <h1> This is About page</h1>
         </div>
     );
